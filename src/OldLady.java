bb7a4fbfa742b195401e2789a6c744092f401199
schoeeva000 <schoeeva000@CAM-LAB-26.bgsd.k12.wa.us>
2019-09-17 07:13:40 -0700

    alldone

--- src/OldLady.java
+++ src/OldLady.java
@@ -4,29 +4,37 @@ public class OldLady {
 	public static void main(String[] args) {
 		fly();
 		idkWhy();
-		System.out.println("There was an old lady who swallowed a spider,\nThat wriggled and iggled and jiggled inside her.");
+		thereWas();
+		System.out.println("spider,\nThat wriggled and iggled and jiggled inside her.");
 		spider();
 		idkWhy();
-		System.out.println("There was an old lady who swallowed a bird,\nHow absurd to swallow a bird.");
+		thereWas();
+		System.out.println("bird,\nHow absurd to swallow a bird.");
 		bird();
 		spider();
 		idkWhy();
-		System.out.println("There was an old lady who swalllowed a cat,\nImagine that to swallow a cat.");
+		thereWas();
+		System.out.println("cat,\nImagine that to swallow a cat.");
 		cat();
 		bird();
 		spider();
 		idkWhy();
-		System.out.println("There was an old lady who swalllowed a dog,\nWhat a hog to swallow a dog.");
+		thereWas();
+		System.out.println("dog,\nWhat a hog to swallow a dog.");
 		dog();
 		cat();
 		bird();
 		spider();
 		idkWhy();
-		System.out.println("There was an old lady who swalllowed a horse,\nShe died of course.");
+		thereWas();
+		System.out.println("horse,\nShe died of course.");
 		
 
 	}
 	
+	public static void thereWas() {
+		System.out.print("There was an old lady who swallowed a ");
+	}
 	public static void idkWhy() {
 		System.out.println("I don't know why she swallowed that fly,");
 		System.out.println("Perhaps she'll die.");
